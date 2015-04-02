3f3cc63c2bfa22116dffc4cd354d78041ff775fe
Vincent Loy <vincent.loy@activkonnect.com>
2015-04-02 12:57:35 +0200

    generation d'un PDF a partir de HTML mais ca plante avec des gros contenues

--- monapp_pdf/views.py
+++ monapp_pdf/views.py
@@ -2,6 +2,18 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from reportlab.pdfgen import canvas
 
+from django import http
+from django.shortcuts import render_to_response
+from django.template.loader import get_template
+from django.template import Context
+import xhtml2pdf.pisa as pisa
+try:
+    import StringIO
+    StringIO = StringIO.StringIO
+except Exception:
+    from io import StringIO
+import cgi
+
 def home(request):
     return render(request, 'app/home.html')
 
@@ -19,3 +31,53 @@ def py_df(request):
     p.save()
 
     return response
+
+#extract from xhtml2pdf sources
+def index(request):
+
+    html = open('monapp_pdf/templates/app/home.html').read()
+
+    return render(request, 'app/index.html', {'html' : html})
+
+def download(request):
+    if request.POST:
+        result = StringIO()
+        pdf = pisa.CreatePDF(
+            StringIO(request.POST["data"]),
+            result
+            )
+        #==============README===================
+        #Django < 1.7 is content_type is mimetype
+        #========================================
+        if not pdf.err:
+            return http.HttpResponse(
+                result.getvalue(),
+                content_type='application/pdf')
+
+    return http.HttpResponse('We had some errors')
+
+def render_to_pdf(template_src, context_dict):
+    template = get_template(template_src)
+    context = Context(context_dict)
+    html  = template.render(context)
+    result = StringIO()
+    pdf = pisa.pisaDocument(StringIO( "{0}".format(html) ), result)
+    if not pdf.err:
+        #==============README===================
+        #Django < 1.7 is content_type is mimetype
+        #========================================
+        return http.HttpResponse(result.getvalue(), content_type='application/pdf')
+    return http.HttpResponse('We had some errors<pre>%s</pre>' % cgi.escape(html))
+
+def ezpdf_sample(request):
+    blog_entries = []
+    for i in range(1,10):
+        blog_entries.append({
+            'id': i,
+            'title':'Playing with pisa 3.0.16 and dJango Template Engine',
+            'body':'This is a simple example..'
+            })
+    return render_to_pdf('entries.html',{
+        'pagesize':'A4',
+        'title':'My amazing blog',
+        'blog_entries':blog_entries})
--- test_pdf/urls.py
+++ test_pdf/urls.py
@@ -4,4 +4,8 @@ from django.contrib import admin
 urlpatterns = patterns('monapp_pdf.views',
     url(r'^home$', 'home'),
     url(r'^reportlab$', 'py_df'),
+
+    url(r'^$', 'index'),
+    url(r'^download', 'download'),
+    url(r'^ezpdf_sample', 'ezpdf_sample'),
 )
